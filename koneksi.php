61647fb96873a07b2cf0f950384706f5e952974a
Anisa Ayu Martalina <anisaayumartalina@gmail.com>
2026-05-09 11:31:41 +0700

    koneksi database

--- koneksi.php
+++ koneksi.php
@@ -8,5 +8,8 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi ke database db_minimarket berhasil!"; 
 }
+
 ?>
