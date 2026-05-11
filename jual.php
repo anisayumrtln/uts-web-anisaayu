be431d4fea4bd22f81c66fbf96dd6be002f4b29d
Anisa Ayu Martalina <anisaayumartalina@gmail.com>
2026-05-11 22:02:24 +0700

    update alur jual biar langsung pindah ke struk

--- jual.php
+++ jual.php
@@ -2,9 +2,7 @@
 include 'koneksi.php';
 $id = $_GET['id'];
 
-// 1. Kurangi stok di database
 mysqli_query($conn, "UPDATE produk SET stok = stok - 1 WHERE id = $id");
 
-// 2. Langsung lempar ke halaman struk biar bisa dicetak
 header("location:struk.php?id=$id");
 ?>
